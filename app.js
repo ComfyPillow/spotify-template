50947cefc9965f1283f15f9883d7147cfae9f85b
George Ng <gngcp@uw.edu>
2015-11-10 02:19:01 -0800

    Completed Basic page

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var data;
+var artistData;
+var relatedData;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var artistData = 'https://api.spotify.com/v1/artists/'
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
@@ -7,11 +10,11 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
-      
     })
-
   }
   $scope.play = function(song) {
+    $scope.clicked = true;
+    $('#artistList').empty('');
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
       $scope.currentSong = false
@@ -25,7 +28,26 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     }
   }
   $scope.artistInfo = function(song) {
-      $scope.artists = song.artists;
+    for (i = 0; i < song.artists.length; i++) {
+
+        $http.get(artistData + song.artists[i].id).success(function(response){
+          $scope.profile = response;
+          var info = $('<li class="name">' + $scope.profile.name + '</li> <img src="' + $scope.profile.images[1].url + '" ng-click="getRelated("' + $scope.profile.id +'");">')
+          $('#artistList').append(info);
+        })
+    }
+      /* $scope.artists = song.artists;
+      $http.get(artistData + song.artists[0].id).success(function(response){
+        $scope.image = response.images[1].url;
+      }) */
+  }
+
+  $scope.getRelated = function(id) {
+    console.log(id);
+    /*$http.get(artistData + id).success(function(response){
+        relatedData = $scope.images = response.images;
+        return relatedData.images[1].url;
+    })*/
   }
 })
 
