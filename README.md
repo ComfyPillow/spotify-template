1b793dfe2fa6383b2799cf6c2dfe5bd8811fe2fa
George Ng <gngcp@uw.edu>
2015-11-10 06:01:35 -0800

    Completed Assignment + ReadME

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # spotify-template
 This template is an optional starting point for a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/spotify).
+
+
+1. What is the purpose of your project (i.e., why would someone use this app)?
+The purpose of this app is to be able to sample music or lookup artists and find related artists who may have similar music. It also provides popularity rankings of the music in regards to the spotify population as a whole. 
+
+2. What is the URL of this project on your student web-server?
+http://students.washington.edu/gngcp/info343/spotify-template/
+
+3. Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+Yes, Jack Whitesell from Lecture A helped me with explaining how to properly interpret the API data as well as advice for other random bugs. He also helped me review a bit of the material from the lectures as I had been sick and absent for over a week. 
+
+4. Approximately how many hours did it take you to complete this challenge?
+16 hours
+- 4~6 was spent learning and understanding the lecture material that I missed
+- approximately 3 was spent coming up and testing ideas (couldn't get echonest to work)
+- the rest went to working and debugging
+
+5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var idData;
+/*  George Ng
+  INFO 343 C
+
+  This is the javascript file used to handle the various angular functions of the
+  spotify song reference and related artists website.*/
+
+var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
 var artistData = 'https://api.spotify.com/v1/artists/'
 var myApp = angular.module('myApp', [])
 
+//This controller grabs the intial searched track data from spotify.
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
   $scope.getSongs = function() {
@@ -11,9 +18,9 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     })
   }
 
+  //This function plays/pauses the music upon clicking the listed songs. 
   $scope.play = function(song) {
     $scope.clicked = true;
-    $('#artistList').empty('');
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
       $scope.currentSong = false
@@ -27,26 +34,17 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     }
   }
 
+  //This function gets the song data such as artist names and albums. 
   $scope.artistInfo = function(song) {
-    var artistID = song.artists[0].id;
-    for (i = 0; i < song.artists.length; i++) {
-        $http.get(artistData + song.artists[i].id).success(function(response){
-          $scope.profile = response;
-          var info = $('<li class="name" ng-click="getRelated("' + $scope.profile.id +'");">' + $scope.profile.name + '</li> <img src="' + $scope.profile.images[1].url + '">')
-          $('#artistList').append(info);
-        })
-    }
-    $scope.getRelated(artistID);
+    $scope.song = song;
+    $scope.artists = song.artists;
+    data = song.artists[0].id;
   }
 
+  //This function gets the related artist data for the selected artist.
   $scope.getRelated = function(artistID) {
-      $http.get(artistData + artistID + "/related-artists").success(function(response){
+      $http.get(artistData + data + "/related-artists").success(function(response){
         $scope.related = response;
       })
   }
 })
-
-// Add tool tips to anything with a title property
-$('body').tooltip({
-    selector: '[title]'
-});
